232a05dfdf0a4446cbf4e413868b0362417fe36e
Ankush <deshwalankush23@gmail.com>
2024-08-17 20:32:32 +0530

    v1 apis modified

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -2,20 +2,34 @@ import { Router, Request, Response } from 'express';
 import { Country, State, City } from "country-state-city"
 const router = Router();
 
-router.get("/countries", (req, res) => {
+router.get("/countries", (req: Request, res: Response) => {
     const countries = Country.getAllCountries()
-    return res.json(countries)
-})
+    const simplifiedCountries = countries.map(country => ({
+        name: country.name,
+        isoCode: country.isoCode
+    }))
+    return res.json(simplifiedCountries)
+});
+
 router.get("/states", (req, res) => {
     const countryCode = req.query.country as string;
     const states = State.getStatesOfCountry(countryCode);
-    return res.json(states);
-})
+    const simplifiedStates = states.map(state => ({
+        name: state.name,
+        isoCode: state.isoCode,
+        countryCode: state.countryCode
+    }));
+    return res.json(simplifiedStates);
+});
+
 router.get("/cities", (req, res) => {
     const countryCode = req.query.country as string;
     const stateCode = req.query.state as string;
     const cities = City.getCitiesOfState(countryCode, stateCode)
-    return res.json(cities)
+    const simplifiedCities = cities.map(city => ({
+        name: city.name,
+    }));
+    return res.json(simplifiedCities)
 })
 
 export default router;
